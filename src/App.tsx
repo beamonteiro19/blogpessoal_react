f35821acad8f31b0e9011354e06b6d0bbced05c8
Beatriz Monteiro Vieira <beatrizmonteirovieira@outlook.com>
2026-01-15 15:01:56 -0300

    troca de pastas no diretorio

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
-import Navbar from "./pages/navbar/Navbar";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
