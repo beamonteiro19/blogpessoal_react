fd2f03369b31144ed226dd7e2a0d2a69383b8bbd
Beatriz Monteiro Vieira <beatrizmonteirovieira@outlook.com>
2026-01-15 17:30:36 -0300

    router dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import "./App.css";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+   <BrowserRouter>
+   <Navbar />
+   {/* distanciamento enrta cada elemento que compoe a pagina */}
+   <div className="min-h-[80vh]">
+   <Routes>
+    <Route path="/" element={<Home />}/>
+    <Route path="/home" element={<Home />}/>
+   </Routes>
+   </div>
+   <Footer/>
+  
+   </BrowserRouter>
     </>
   );
 }
