43c88fbe3d1dcaed72b3d79415601b7655474554
Beatriz Monteiro Vieira <beatrizmonteirovieira@outlook.com>
2026-01-23 13:18:53 -0300

    arquivo faltante

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,28 +1,47 @@
-import { FacebookLogoIcon, GithubLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react"
-
+import {
+  FacebookLogoIcon,
+  GithubLogoIcon,
+  InstagramLogoIcon,
+  LinkedinLogoIcon,
+} from "@phosphor-icons/react";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useContext, type ReactNode } from "react";
 
 function Footer() {
+  let data = new Date().getFullYear();
 
-    let data = new Date().getFullYear()
+  const { usuario } = useContext(AuthContext);
 
-    return (
-        <>
-            <div className="flex justify-center bg-indigo-900 text-white">
-                <div className="container flex flex-col items-center py-4">
-                    <p className='text-xl font-bold'>
-                            Blog Pessoal Generation | Copyright: {data}
-                        </p>
-                    <p className='text-lg'>Acesse minhas redes sociais</p>
-                    <div className='flex gap-2'>
-                       <a href="https://www.linkedin.com/in/beatriz-mv/"><LinkedinLogoIcon size={48} weight='bold' /></a>
-                       <a href="https://www.instagram.com/_beaaamonteiro/"><InstagramLogoIcon size={48} weight='bold' /></a> 
-                        <a href="https://www.facebook.com/beatrizmonteirovieira/"><FacebookLogoIcon size={48} weight='bold' /></a> 
-                        <a href="https://github.com/beamonteiro19"><GithubLogoIcon size={48} weight='bold' /></a> 
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  let component: ReactNode;
+
+  if (usuario.token !== "") {
+    component = (
+      <div className="flex justify-center bg-indigo-900 text-white">
+        <div className="container flex flex-col items-center py-4">
+          <p className="text-xl font-bold">
+            Blog Pessoal Generation | Copyright: {data}
+          </p>
+          <p className="text-lg">Acesse minhas redes sociais</p>
+          <div className="flex gap-2">
+            <a href="https://www.linkedin.com/in/beatriz-mv/">
+              <LinkedinLogoIcon size={48} weight="bold" />
+            </a>
+            <a href="https://www.instagram.com/_beaaamonteiro/">
+              <InstagramLogoIcon size={48} weight="bold" />
+            </a>
+            <a href="https://www.facebook.com/beatrizmonteirovieira/">
+              <FacebookLogoIcon size={48} weight="bold" />
+            </a>
+            <a href="https://github.com/beamonteiro19">
+              <GithubLogoIcon size={48} weight="bold" />
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return <>{component}</>;
 }
 
-export default Footer
+export default Footer;
